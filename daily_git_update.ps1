9f81711c5246fdd9a29338e27caa2921a89e8f3d
unknown <osadon2060@gmail.com>
2024-11-17 21:49:45 +0200

    הוספת קבצים חדשים ועדכון יומי

---
+++ daily_git_update.ps1
@@ -0,0 +1,10 @@
+cd C:\Users\osado\AppData\Local\WebGrab+Plus
+
+# הוספת קובץ למעקב
+git add guide.xml
+
+# יצירת Commit
+git commit -m "Daily update for guide.xml"
+
+# שליחה ל-Remote
+git push -u origin main
---
+++ run_webgrab.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\admin\AppData\Local\WebGrab+Plus
+start Run
+exit
